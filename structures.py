0ec09dfc848472589804cd498bbc90a81009322d
Soy19 <96676396+Soy19@users.noreply.github.com>
2022-05-16 20:34:14 +0900

    Create structures.py
    made only two structures. Try to make it by 5/20 9:00 JST

---
+++ structures.py
@@ -0,0 +1,26 @@
+# First data structure arrays
+abc = ["x","b","c","d"]
+print(abc[0])
+print(abc[1])
+print(abc[2])
+print(abc[3])
+abc[0] = "a"
+len(abc)
+# the len command in python will get the number of elements in any array
+
+num = []
+  
+# append() function to push
+# element in the stack
+num.append('a')
+num.append('b')
+num.append('c')
+
+print(num)
+
+print(stack.pop())
+print(stack.pop())
+print(stack.pop())
+  
+print('Stack after elements are popped:')
+print(stack)
