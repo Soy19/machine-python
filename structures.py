c1d1def5927c3706f68a10dda8c01173831c6ffb
Soy19 <96676396+Soy19@users.noreply.github.com>
2022-05-18 17:51:35 +0900

    Update!

--- structures.py
+++ structures.py
@@ -1,5 +1,5 @@
 # First data structure arrays
-abc = ["x","b","c","d"]
+abc = ["x", "b", "c", "d"]
 print(abc[0])
 print(abc[1])
 print(abc[2])
@@ -9,7 +9,7 @@ len(abc)
 # the len command in python will get the number of elements in any array
 
 num = []
-  
+
 # append() function to push
 # element in the stack
 num.append('a')
@@ -18,9 +18,28 @@ num.append('c')
 
 print(num)
 
-print(stack.pop())
-print(stack.pop())
-print(stack.pop())
-  
+print(num.pop())
+print(num.pop())
+print(num.pop())
+
 print('Stack after elements are popped:')
-print(stack)
+print(num)
+
+# dictionary
+
+ao = {
+    'y':3,
+    'z':int('y') ** 2,
+    'a':(int('z') - int('y')),
+    'b':(int('z') + int('y')),
+    'c':(int('z')*int('y')),
+    'd':(int('z') / int('y')),
+}
+print(ao)
+
+
+# Declare a dictionary
+aBz = {'Name': 'Taro', 'Age': 12, 'Class': '1-C', 'Results': '78'}
+
+print("The name is," + aBz['Name'])
+print("The age of" + aBz['Name'] + "is" + aBz['Age'])
